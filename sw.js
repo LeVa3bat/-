8688dc206ccc20107ee3013cca60ef8b7a81b999
LeVa3bat <alex.666.881@gmail.com>
2025-09-16 10:35:00 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('gvozdika-cache').then((cache) => cache.addAll([
+    caches.open('gvozdika-cache-v1').then((cache) => cache.addAll([
       '/',
       '/index.html',
-      // Добавьте другие файлы для кэширования, если есть (CSS, JS, изображения)
+      '/manifest.json',
+      '/sw.js',
+      '/icon-192.png', // Убедитесь, что иконки доступны
+      '/icon-512.png'
     ]))
   );
 });
@@ -11,5 +14,15 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => Promise.all(
+      keyList.map((key) => {
+        if (key !== 'gvozdika-cache-v1') return caches.delete(key);
+      })
+    ))
   );
 });
